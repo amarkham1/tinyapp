ea2d0b41388b4cb18cc546cecc6bbce57a97c75b
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-28 20:28:57 +0000

    implemented random ID function

--- express_server.js
+++ express_server.js
@@ -6,6 +6,10 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -23,6 +27,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
